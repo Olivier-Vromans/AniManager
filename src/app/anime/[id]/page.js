652bdc1bee3f16b2b7edc897603cbfd1178b2fe8
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-01 18:07:55 +0200

    Anime details started

--- src/app/anime/[id]/page.js
+++ src/app/anime/[id]/page.js
@@ -1,12 +1,49 @@
+"use client";
+import Image from "next/image.js";
+import { useState, useEffect, useCallback } from "react";
 
-export default function AnimeDetail({params}) {
-    console.log(params);
-    return (
-      <main>
-        <div className="flex flex-col items-center justify-between p-24">
-          <h1 className='text-center text-4xl'>This is the anime detail page of {params.id}</h1>
+export default function AnimeDetail({ params }) {
+  // console.log(params);
+  const [y, setY] = useState(0);
+
+  const [series, setSeries] = useState({
+    title: 'Code Geass',
+    poster: '/img/webp/series/code-geass/poster.webp',
+    banner: '/img/webp/series/code-geass/banner.jpg',
+    genres: [],
+    order: 0,
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setY(Math.min(window.scrollY, 320));
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <main>
+      <div className="container">
+        <div
+          className="flex flex-col items-center justify-between mb-24 mt-48 w-full h-80 bg-cover bg-no-repeat bg-[url('/img/webp/series/code-geass/banner.jpg')]"
+          style={{ backgroundPositionY: `calc(-${y}px)` }}
+        >
+          {/* Add your banner content here */}
         </div>
-      </main>
-    );
-  }
-  
+        <div className="container flex flex-row items-center mb-24">
+          <div className="flex-initial">
+            <Image className='' src={series.poster} alt={series.title} width={400} height={500} />
+          </div>
+          <div className="flex-1 ml-6">
+            <h2 className='text-2xl'>Code Geass</h2>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
