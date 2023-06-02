e3b40df022607f5819cf430799121748252b9430
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-02 23:05:52 +0200

    detail page done (kind of)

--- src/app/anime/[id]/page.js
+++ src/app/anime/[id]/page.js
@@ -3,44 +3,171 @@ import Image from "next/image.js";
 import { useState, useEffect, useCallback } from "react";
 
 export default function AnimeDetail({ params }) {
-  // console.log(params);
-  const [y, setY] = useState(0);
-
   const [series, setSeries] = useState({
     title: 'Code Geass',
-    poster: '/img/webp/series/code-geass/poster.webp',
-    banner: '/img/webp/series/code-geass/banner.jpg',
+    anime: [
+      {
+        id: 1,
+        title: 'Lelouch of the Rebellion',
+        poster: '/img/webp/anime/lelouch-of-the-rebellion/poster.webp',
+        banner: '/img/webp/anime/lelouch-of-the-rebellion/banner.webp',
+        genres: [],
+        episodes: [
+          {
+            id: 1,
+            title: 'The Day a New Demon was Born',
+            episode: 1,
+            filler: false,
+            dubbed: true,
+          },
+          {
+            id: 2,
+            title: 'The White Knight Awakens',
+            episode: 2,
+            filler: false,
+            dubbed: true,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Lelouch of the Rebellion R2',
+        poster: '/img/webp/anime/lelouch-of-the-rebellion-r2/poster.webp',
+        banner: '/img/webp/anime/lelouch-of-the-rebellion-r2/banner.webp',
+        genres: [],
+        episodes: [
+          {
+            id: 1,
+            title: 'The Day a New Demon was Born',
+            episode: 1,
+            filler: false,
+            dubbed: true,
+          },
+          {
+            id: 2,
+            title: 'The White Knight Awakens',
+            episode: 2,
+            filler: false,
+            dubbed: true,
+          },
+        ],
+      },
+      // Add more anime objects if needed
+    ],
     genres: [],
     order: 0,
   });
 
+  const [banner, setBanner] = useState();
+  const [poster, setPoster] = useState();
+
+  // Randomize the banner and poster images
+  useEffect(() => {
+    const randomizeImages = () => {
+      const anime = series.anime;
+      const randomBannerIndex = Math.floor(Math.random() * anime.length);
+      const randomPosterIndex = Math.floor(Math.random() * anime.length);
+      setBanner(anime[randomBannerIndex].banner);
+      setPoster(anime[randomPosterIndex].poster);
+    };
+
+    randomizeImages();
+  }, []);
+
+  // Determine if the user is scrolling up or down and adjust the banner accordingly
+  const [y, setY] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       setY(Math.min(window.scrollY, 320));
     };
 
-    window.addEventListener('scroll', handleScroll);
+    if (!isMobile) {
+      window.addEventListener('scroll', handleScroll);
+    }
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
+  }, [isMobile]);
+
+  // Determine if the user is on mobile or not
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint if needed
+    };
+
+    handleResize(); // Set initial screen size
+    window.addEventListener('resize', handleResize); // Update screen size on resize
+
+    return () => {
+      window.removeEventListener('resize', handleResize); // Clean up event listener
+    };
   }, []);
+
+  const [activeFilter, setActiveFilter] = useState('release');
+
+  const Filter = ({ activeFilter, setActiveFilter }) => {
+    return (
+      <div id="filters" className="flex flex-row items-center justify-center md:justify-start w-full mb-4">
+        <button
+          className={`${activeFilter === 'release' ? 'btn' : 'btn-inactive'} mr-4 py-2 px-4 rounded-sm`}
+          onClick={() => setActiveFilter('release')}
+        >
+          Release
+        </button>
+        <button
+          className={`${activeFilter === 'chronicle' ? 'btn' : 'btn-inactive'} mr-4 py-2 px-4 rounded-sm`}
+          onClick={() => setActiveFilter('chronicle')}
+        >
+          Chronicle
+        </button>
+        <button
+          className={`${activeFilter === 'community' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
+          onClick={() => setActiveFilter('community')}
+        >
+          Community
+        </button>
+      </div>
+    );
+  };
+  
 
   return (
     <main>
       <div className="container">
         <div
-          className="flex flex-col items-center justify-between mb-24 mt-48 w-full h-80 bg-cover bg-no-repeat bg-[url('/img/webp/series/code-geass/banner.jpg')]"
-          style={{ backgroundPositionY: `calc(-${y}px)` }}
-        >
-          {/* Add your banner content here */}
-        </div>
-        <div className="container flex flex-row items-center mb-24">
-          <div className="flex-initial">
-            <Image className='' src={series.poster} alt={series.title} width={400} height={500} />
+          className="flex flex-col items-center justify-between mb-12 mt-28 w-full h-36 bg-cover bg-no-repeat"
+          style={{
+            backgroundPositionY: `calc(-${y}px)`,
+            backgroundImage: `url(${banner})`
+          }}
+        ></div>
+        <div className="container flex flex-row mb-24">
+          <div id="poster" className="flex-initial">
+            <Image className="md:mb-4 hidden md:block" src={poster} alt={series.title} width={250} height={350} />
+            {!isMobile && <Filter activeFilter={activeFilter} setActiveFilter={setActiveFilter} />}
           </div>
-          <div className="flex-1 ml-6">
-            <h2 className='text-2xl'>Code Geass</h2>
+          <div id="order" className="flex-1 md:ml-6">
+            <p className='font-japanese text-center text-4xl md:mb-8'>{series.title} Watch Order</p>
+            {isMobile && <Filter activeFilter={activeFilter} setActiveFilter={setActiveFilter} />}
+            <div className="flex flex-col flex-wrap items-center justify-center md:justify-start w-full pb-8">
+              {series.anime.map((anime) => (
+                <div className="flex flex-row items-center justify-center w-full mb-4 sm:mb-0 md:m-6">
+                  <Image
+                    className='w-32 h-32 md:w-1/2 lg:w-1/6 object-contain'
+                    src={anime.poster}
+                    alt={anime.title}
+                    width={100}
+                    height={100}
+                    loader={({ src, width, quality }) => `${src}?w=${width}&q=${quality || 75}`} />
+                  <div className='flex-1'>
+                    <p className='text-center md:text-start md:text-2xl'>{anime.title}</p>
+                    <p className="text-center md:text-start md:text-xl text-subtext font-extrabold">{`Episode ${anime.episodes[0].episode} - ${anime.episodes[anime.episodes.length - 1].episode}`}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
