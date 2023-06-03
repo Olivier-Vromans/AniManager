2c36bbb2bc8e1f60ac747c4205334c1516c104e3
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-03 02:42:44 +0200

    quick fixes

--- src/app/anime/[id]/page.js
+++ src/app/anime/[id]/page.js
@@ -1,81 +1,109 @@
 "use client";
-import Image from "next/image.js";
+import Image from "next/image";
 import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
+
+
+async function getData() {
+  const fetchedSeries = [
+    {
+      id: 1,
+      title: "Code Geass",
+      anime: [
+        {
+          id: 1,
+          title: "Lelouch of the Rebellion",
+          poster: "/img/webp/anime/lelouch-of-the-rebellion/poster.webp",
+          banner: "/img/webp/anime/lelouch-of-the-rebellion/banner.webp",
+          genres: [],
+          episodes: [
+            {
+              id: 1,
+              title: "The Day a New Demon was Born",
+              episode: 1,
+              filler: false,
+              dubbed: true,
+            },
+            {
+              id: 2,
+              title: "The White Knight Awakens",
+              episode: 2,
+              filler: false,
+              dubbed: true,
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "Lelouch of the Rebellion R2",
+          poster: "/img/webp/anime/lelouch-of-the-rebellion-r2/poster.webp",
+          banner: "/img/webp/anime/lelouch-of-the-rebellion-r2/banner.webp",
+          genres: [],
+          episodes: [
+            {
+              id: 1,
+              title: "The Day a New Demon was Born",
+              episode: 1,
+              filler: false,
+              dubbed: true,
+            },
+            {
+              id: 2,
+              title: "The White Knight Awakens",
+              episode: 2,
+              filler: false,
+              dubbed: true,
+            },
+          ],
+        },
+      ],
+      genres: [],
+      order: 0,
+    },
+  ];
+  return fetchedSeries;
+}
 
 export default function AnimeDetail({ params }) {
-  const [activeFilter, setActiveFilter] = useState('release');
-  const [series, setSeries] = useState({
-    title: 'Code Geass',
-    anime: [
-      {
-        id: 1,
-        title: 'Lelouch of the Rebellion',
-        poster: '/img/webp/anime/lelouch-of-the-rebellion/poster.webp',
-        banner: '/img/webp/anime/lelouch-of-the-rebellion/banner.webp',
-        genres: [],
-        episodes: [
-          {
-            id: 1,
-            title: 'The Day a New Demon was Born',
-            episode: 1,
-            filler: false,
-            dubbed: true,
-          },
-          {
-            id: 2,
-            title: 'The White Knight Awakens',
-            episode: 2,
-            filler: false,
-            dubbed: true,
-          },
-        ],
-      },
-      {
-        id: 2,
-        title: 'Lelouch of the Rebellion R2',
-        poster: '/img/webp/anime/lelouch-of-the-rebellion-r2/poster.webp',
-        banner: '/img/webp/anime/lelouch-of-the-rebellion-r2/banner.webp',
-        genres: [],
-        episodes: [
-          {
-            id: 1,
-            title: 'The Day a New Demon was Born',
-            episode: 1,
-            filler: false,
-            dubbed: true,
-          },
-          {
-            id: 2,
-            title: 'The White Knight Awakens',
-            episode: 2,
-            filler: false,
-            dubbed: true,
-          },
-        ],
-      },
-      // Add more anime objects if needed
-    ],
-    genres: [],
-    order: 0,
-  });
+  const [activeFilter, setActiveFilter] = useState("release");
+  const [series, setSeries] = useState(null);
 
-  
-  const [banner, setBanner] = useState();
-  const [poster, setPoster] = useState();
+  useEffect(() => {
+    async function fetchSeries() {
+      const data = await getData();
+      const foundSeries = data.find((series) => series.id === parseInt(params.id));
+      setSeries(foundSeries);
+      console.log(foundSeries);
+    }
+    fetchSeries();
+  }, [params.id]);
 
   const [y, setY] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
-  
-  useEffect(() => {
-    // Randomize the banner and poster images
-    const randomizeImages = () => {
-      const anime = series.anime;
-      const randomBannerIndex = Math.floor(Math.random() * anime.length);
-      const randomPosterIndex = Math.floor(Math.random() * anime.length);
-      setBanner(anime[randomBannerIndex].banner);
-      setPoster(anime[randomPosterIndex].poster);
-    };
 
+
+  const [randomBannerIndex, setRandomBannerIndex] = useState(0);
+  const [randomPosterIndex, setRandomPosterIndex] = useState(0);
+
+  // Randomize the banner and poster images
+  const randomizeImages = () => {
+    if (series && series.anime.length > 0) {
+      const bannerIndex = Math.floor(Math.random() * series.anime.length);
+      const posterIndex = Math.floor(Math.random() * series.anime.length);
+      const anime = series.anime;
+      setRandomBannerIndex(bannerIndex);
+      setRandomPosterIndex(posterIndex);
+    }
+  };
+
+  useEffect(() => {
+    randomizeImages();
+
+    // Rest of the code...
+  }, [series]);
+
+  useEffect(() => {
     // Determine if the user is scrolling up or down and adjust the banner accordingly
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint if needed
@@ -88,8 +116,7 @@ export default function AnimeDetail({ params }) {
       window.removeEventListener('resize', handleResize); // Clean up event listener
     };
 
-    randomizeImages();
-  });
+  }), [series];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -135,6 +162,14 @@ export default function AnimeDetail({ params }) {
   };
 
 
+  if (!series) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="text-2xl">Loading...</div>
+      </div>
+    );
+  }
+
   return (
     <main>
       <div className="container">
@@ -142,33 +177,42 @@ export default function AnimeDetail({ params }) {
           className="flex flex-col items-center justify-between mb-12 mt-28 w-full h-36 bg-cover bg-no-repeat"
           style={{
             backgroundPositionY: `calc(-${y}px)`,
-            backgroundImage: `url(${banner})`
+            backgroundImage: series ? `url(${series.anime[randomBannerIndex].banner})` : "none"
           }}
-        ></div>
+        />
         <div className="container flex flex-row mb-24">
           <div id="poster" className="flex-initial">
-            <Image className="md:mb-4 hidden md:block w-full" src={poster} alt={series.title} width={250} height={350} />
+              <Image
+                className="md:mb-4 hidden md:block w-full"
+                src={series.anime[randomPosterIndex].poster}
+                alt={series.title}
+                width={250}
+                height={350}
+              />
             {!isMobile && <Filter activeFilter={activeFilter} setActiveFilter={setActiveFilter} />}
           </div>
           <div id="order" className="flex-1 md:ml-6">
-            <p className='font-japanese text-center text-4xl md:mb-8'>{series.title} Watch Order</p>
+              <p className="font-japanese text-center text-4xl md:mb-8">{series.title} Watch Order</p>
             {isMobile && <Filter activeFilter={activeFilter} setActiveFilter={setActiveFilter} />}
             <div className="flex flex-col flex-wrap items-center justify-center md:justify-start w-full pb-8">
               {series.anime.map((anime) => (
-                <div key={anime.id} className="flex flex-row items-center justify-center w-full mb-4 sm:mb-0 md:m-6">
-                  <Image
-                    className='w-32 h-32 md:w-1/2 lg:w-1/6 object-contain'
-                    src={anime.poster}
-                    alt={anime.title}
-                    width={100}
-                    height={100}
-                    loader={({ src, width, quality }) => `${src}?w=${width}&q=${quality || 75}`} />
-                  <div className='flex-1'>
-                    <p className='text-center md:text-start md:text-2xl'>{anime.title}</p>
-                    <p className="text-center md:text-start md:text-xl text-subtext font-extrabold">{`Episode ${anime.episodes[0].episode} - ${anime.episodes[anime.episodes.length - 1].episode}`}</p>
+                  <div key={anime.id} className="flex flex-row items-center justify-center w-full mb-4 sm:mb-0 md:m-6">
+                    <Image
+                      className="w-32 h-32 md:w-1/2 lg:w-1/6 object-contain"
+                      src={anime.poster}
+                      alt={anime.title}
+                      width={100}
+                      height={100}
+                      loader={({ src, width, quality }) => `${src}?w=${width}&q=${quality || 75}`}
+                    />
+                    <div className="flex-1">
+                      <p className="text-center md:text-start md:text-2xl">{anime.title}</p>
+                      <p className="text-center md:text-start md:text-xl text-subtext font-extrabold">
+                        {`Episode ${anime.episodes[0].episode} - ${anime.episodes[anime.episodes.length - 1].episode}`}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { FaBars, FaSearch, FaUser } from 'react-icons/fa';
+import { FaBars, FaDatabase, FaSearch, FaUser } from 'react-icons/fa';
 import { metadata } from '@/app/layout.js';
 
 export default function Nav() {
     return (
-        <nav className='absolute flex flex-row items-center justify-between left-0 right-0 top-6 md:top-16 px-6 md:px-32'>
+        <nav className='absolute flex flex-row items-center justify-between left-0 right-0 top-6 md:top-10 px-6 md:px-32'>
             <a href='/' className='mx-2 md:mx-3'>
                 <h1 className='font-japanese text-xl md:text-2xl ml-auto mr-auto w-full'>{metadata.title}</h1>
             </a>
             <div className='flex flex-row items-center space-x-2 md:space-x-3'>
-                <a href='/' className='mx-2 md:mx-3'>
-                    <FaSearch color='#808080' size={25} />
+                {/* TODO Here can go the nav Items */}
+                <a href='/' className='mx-2 md:mx-3 object-center flex flex-col items-center'>
+                    {/* <FaDatabase color='#808080' size={25} /> */}
                 </a>
-                <a href='/' className='mx-2 md:mx-3'>
+                {/* <a href='/' className='mx-2 md:mx-3'>
                     <FaUser color='#808080' size={25} />
                 </a>
                 <a href='/' className='mx-2 md:mx-3'>
                     <FaBars color='#808080' size={25} />
-                </a>
+                </a> */}
             </div>
         </nav>
     );
