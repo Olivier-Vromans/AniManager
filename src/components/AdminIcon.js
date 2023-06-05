bba2bb49ccd071d2a9b321ff79fed43dda4d9320
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-05 23:37:06 +0200

    Admin Icon for navbar

---
+++ src/components/AdminIcon.js
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import { useAuth } from '../../context/AuthContext.js';
+import { FaUser } from 'react-icons/fa';
+
+export default function AdminIcon() {
+    const { currentUser } = useAuth();
+
+    console.log(currentUser);
+    return currentUser && <a href='/admin' className='mx-2 md:mx-3 object-center flex flex-col items-center'>
+        <FaUser color='#fff' size={25} />
+    </a>
+
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FaBars, FaDatabase, FaSearch, FaUser } from 'react-icons/fa';
 import { metadata } from '@/app/layout.js';
+import AdminIcon from './AdminIcon.js';
 
 export default function Nav() {
+
     return (
-        <nav className='absolute flex flex-row items-center justify-between left-0 right-0 top-6 md:top-10 px-6 md:px-32'>
+        <nav className='absolute z-50 flex flex-row items-center justify-between left-0 right-0 top-6 md:top-10 px-6 md:px-32'>
             <a href='/' className='mx-2 md:mx-3'>
                 <h1 className='font-japanese text-xl md:text-2xl ml-auto mr-auto w-full'>{metadata.title}</h1>
             </a>
@@ -13,6 +15,7 @@ export default function Nav() {
                 <a href='/' className='mx-2 md:mx-3 object-center flex flex-col items-center'>
                     {/* <FaDatabase color='#808080' size={25} /> */}
                 </a>
+                {/* <AdminIcon /> */}
                 {/* <a href='/' className='mx-2 md:mx-3'>
                     <FaUser color='#808080' size={25} />
                 </a>
