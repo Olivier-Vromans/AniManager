b6f3cf0b2b29be3be5797f9aadab26e3c6fad228
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-07 13:18:18 +0200

    added typescript api route

--- pages/api/orders.ts
+++ pages/api/orders.ts
@@ -1,8 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
   try {
     await prisma.$connect();
     const series = await prisma.series.findMany({
