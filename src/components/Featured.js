dcf11abf90020bbf0a4feb9b7e2dcfb71ec760e2
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-12 18:32:13 +0200

    add dynamic featured

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,24 +1,38 @@
-"use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card.js'
+import axios from 'axios';
 
 export default function Featured() {
-    // fetch the featured anime from the database with prisma
+    const [featuredSeries, setFeaturedSeries] = useState(null)
+    const [fetching, setFetching] = useState(true)
 
+    useEffect(() => {
+        async function fetchViewingOrders() {
+            try {
+                const response = await axios.get("/api/featured");
+                setFeaturedSeries(response.data);
+                setFetching(false);
+                console.log(response.data)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchViewingOrders();
+    }, []);
+
+    if (fetching) {
+        return <div id='featured' className="z-10 flex flex-row items-center justify-between w-full overflow-x-scroll sm:overflow-hidden">
+            Loading...
+        </div>;
+    }
 
     return (
         <div id='featured' className="z-10 flex flex-row items-center justify-between w-full overflow-x-scroll sm:overflow-hidden">
-            {/* TODO Change to dynamic feature */}
-            <div className="flex-shrink-0 mx-2 sm:mx-5">
-                <Card img={"anime/demon-slayer/demon-slayer"} title={"Demon Slayer"} id={1} />
-            </div>
-            <div className="flex-shrink-0 mx-2 sm:mx-5">
-                <Card img={"anime/naruto/naruto"} title={"Naruto"} id={2} />
-            </div>
-            <div className="flex-shrink-0 mx-2 sm:mx-5">
-                <Card img={"anime/code-geass/lelouch-of-the-rebellion/poster"} title={"Code Geass"} id={3} />
-            </div>
+            {featuredSeries.map((serie) => (
+                <a href={`serie/${serie.serie_id}`} key={serie.serie_id} className="flex-shrink-0 mx-2 sm:mx-5">
+                    <Card img={serie.poster ? serie.poster : serie.animes[0].poster} title={serie.serie_name} id={serie.serie_id} />
+                </a>
+            ))}
         </div>
-
     )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Featured from './Featured.js';
 
+
 export default function Hero() {
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[url('/img/webp/mobile-hero.webp')] md:bg-[url('/img/png/itachi.png')] bg-no-repeat bg-top bg-cover md:bg-no-repeat md:bg-cover">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[url('/img/bg/mobile-hero.webp')] md:bg-[url('/img/bg/itachi.png')] bg-no-repeat bg-top bg-cover md:bg-no-repeat md:bg-cover">
       <div className=" w-full justify-center items-center font-mono text-sm sm:flex">
         <div className="flex flex-col lg:flex-row items-center">
           <div className="flex flex-col text-center items-center w-full p-4 sm:p-24">
@@ -11,7 +13,7 @@ export default function Hero() {
               Check out the right way to watch Anime
             </h1>
             <a href='/anime' className='btn-secondary hover:opacity-100 uppercase mb-4 sm:mt-4' >
-                Browse the library
+              Browse the library
             </a>
           </div>
           <Featured />
