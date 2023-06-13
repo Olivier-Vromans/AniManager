d1c1318ef44fae6c9a39680035562bf30c27b70d
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-13 23:30:36 +0200

    edit refactor and change to new database

--- src/app/series/[id]/page.js
+++ src/app/series/[id]/page.js
@@ -14,13 +14,14 @@ async function getData(serieId) {
 }
 
 export default function AnimeDetail({ params }) {
-  const [activeFilter, setActiveFilter] = useState("release");
+  const [activeFilter, setActiveFilter] = useState("Release");
   const [series, setSeries] = useState(null);
 
   useEffect(() => {
     async function fetchSeries() {
       try {
         const data = await getData(params.id);
+        console.log(data)
         if (data && data.anime) {
           data.anime.forEach((anime) => {
             const seriesTitle = data.title.toLowerCase().replace(/\s/g, "-");
@@ -39,26 +40,42 @@ export default function AnimeDetail({ params }) {
   }, [params.id]);
 
   const seriesName = useMemo(() => series ? series.serie_name.toLowerCase().replace(/\s/g, '-') : '', [series]);
-
   const randomBanner = useMemo(() => {
     if (series && series.animes.length > 0) {
-      const randomBannerIndex = Math.floor(Math.random() * series.animes.length);
-      const animeTitle = series.animes[randomBannerIndex].title.toLowerCase().replace(/\s/g, '-');
-      return `/img/anime/${seriesName}/${animeTitle}/${series.animes[randomBannerIndex].banner}`;
+      let randomBannerIndex = Math.floor(Math.random() * series.animes.length);
+      let animeTitle = series.animes[randomBannerIndex].title.toLowerCase().replace(/\s/g, '-');
+      let banner = series.animes[randomBannerIndex].banner;
+
+      // Retry with a different random index if the banner is empty
+      while (!banner) {
+        randomBannerIndex = Math.floor(Math.random() * series.animes.length);
+        animeTitle = series.animes[randomBannerIndex].title.toLowerCase().replace(/\s/g, '-');
+        banner = series.animes[randomBannerIndex].banner;
+      }
+
+      return `/img/anime/${seriesName}/${animeTitle}/${banner}`;
     }
+
     return '';
   }, [series, seriesName]);
 
+
   const randomPoster = useMemo(() => {
     if (series && series.animes.length > 0) {
-      const randomPosterIndex = Math.floor(Math.random() * series.animes.length);
-      const animeTitle = series.animes[randomPosterIndex].title.toLowerCase().replace(/\s/g, '-');
+      let randomPosterIndex = Math.floor(Math.random() * series.animes.length);
+      let animeTitle = series.animes[randomPosterIndex].title.toLowerCase().replace(/\s/g, '-');
+      let poster = series.animes[randomPosterIndex].poster;
+
+      while (!poster) {
+        randomPosterIndex = Math.floor(Math.random() * series.animes.length);
+        animeTitle = series.animes[randomPosterIndex].title.toLowerCase().replace(/\s/g, '-');
+        poster = series.animes[randomPosterIndex].poster;
+      }
+
       return `/img/anime/${seriesName}/${animeTitle}/${series.animes[randomPosterIndex].poster}`;
     }
     return '';
   }, [series, seriesName]);
-
-
 
   const [y, setY] = useState(0);
   const [isMobile, setIsMobile] = useState(false);
@@ -79,7 +96,7 @@ export default function AnimeDetail({ params }) {
 
   useEffect(() => {
     const handleScroll = () => {
-      setY(Math.min(window.scrollY, 320));
+      setY(Math.min(window.scrollY, 480));
     };
 
     if (!isMobile) {
@@ -97,20 +114,20 @@ export default function AnimeDetail({ params }) {
         <p className="hidden sm:block text-lg font-gilroy text-subtext">Order</p>
         <div className="flex flex-row items-center justify-around w-full mb-4">
           <button
-            className={`${activeFilter === 'release' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
-            onClick={() => setActiveFilter('release')}
+            className={`${activeFilter === 'Release' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
+            onClick={() => setActiveFilter('Release')}
           >
             Release
           </button>
           <button
-            className={`${activeFilter === 'chronicle' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
-            onClick={() => setActiveFilter('chronicle')}
+            className={`${activeFilter === 'Chronological' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm mx-4`}
+            onClick={() => setActiveFilter('Chronological')}
           >
             Chronicle
           </button>
           <button
-            className={`${activeFilter === 'community' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
-            onClick={() => setActiveFilter('community')}
+            className={`${activeFilter === 'Community' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
+            onClick={() => setActiveFilter('Community')}
           >
             Community
           </button>
@@ -133,7 +150,7 @@ export default function AnimeDetail({ params }) {
         <div
           className="flex flex-col items-center justify-between mb-12 mt-28 w-full h-36 bg-cover bg-no-repeat"
           style={{
-            backgroundPositionY: `calc(-${y}px)`,
+            backgroundPositionY: `calc(40% + -${y / 2}px)`,
             backgroundImage: series ? `url(${randomBanner})` : "none"
           }}
         />
@@ -152,24 +169,38 @@ export default function AnimeDetail({ params }) {
             <p className="font-japanese text-center text-4xl md:mb-8">{series.title} Watch Order</p>
             {isMobile && <Filter activeFilter={activeFilter} setActiveFilter={setActiveFilter} />}
             <div className="flex flex-col flex-wrap items-center justify-center md:justify-start w-full pb-8">
-              {series.animes.map((anime) => (
-                <div key={anime.anime_id} className="flex flex-row items-center justify-center w-full mb-4 sm:mb-0 md:m-6">
-                  <Image
-                    className="w-32 h-32 md:w-1/2 lg:w-1/6 object-contain"
-                    src={`/img/anime/${seriesName}/${anime.title.toLowerCase().replace(/\s/g, '-')}/${anime.poster}`}
-                    alt={`${anime.title} Poster`}
-                    width={100}
-                    height={100}
-                    loader={({ src, width, quality }) => `${src}?w=${width}&q=${quality || 75}`}
-                  />
-                  <div className="flex-1">
-                    <p className="text-center md:text-start md:text-2xl">{anime.title}</p>
-                    <p className="text-center md:text-start md:text-xl text-subtext font-extrabold">
-                      {`Episode ${anime.episodes[0].episode_number} - ${anime.episodes[anime.episodes.length - 1].episode_number}`}
-                    </p>
-                  </div>
-                </div>
-              ))}
+              {/* Display anime order based on activeFilter */}
+              {series.seriesOrder.map((order) => {
+                console.log(order.order_type);
+                console.log(activeFilter);
+                console.log(order.seriesOrderItems);
+                if (order.order_type === activeFilter) {
+                  return order.seriesOrderItems.map((serieOrder) => {
+                    console.log(serieOrder.anime);
+                    return (
+                      <div key={serieOrder.anime.anime_id} className="flex flex-row items-center justify-center w-full mb-4 sm:mb-0 md:m-6">
+                        <Image
+                          className="w-32 h-32 md:w-1/2 lg:w-1/6 object-contain"
+                          src={serieOrder.anime.poster ? `/img/anime/${series.serie_name.toLowerCase().replace(/\s/g, '-')}/${serieOrder.anime.title.toLowerCase().replace(/\s/g, '-')}/${serieOrder.anime.poster}` : `/img/anime/${series.serie_name.toLowerCase().replace(/\s/g, '-')}/${series.poster}`}
+                          width={100}
+                          height={100}
+                          loader={({ src, width, quality }) => `${src}?w=${width}&q=${quality || 75}`}
+                        />
+                        <div className="flex-1">
+                          <p className="text-center md:text-start md:text-2xl">{serieOrder.anime.title}</p>
+                          <p className="text-center md:text-start md:text-xl text-subtext font-extrabold">
+                            { serieOrder.fromEpisode !== serieOrder.toEpisode ?
+                            `Episode ${serieOrder.fromEpisode} - ${serieOrder.toEpisode}` :
+                            `Episode ${serieOrder.fromEpisode}`
+                            }
+                          </p>
+                        </div>
+                      </div>
+                    );
+                  });
+                }
+              })
+              }
             </div>
           </div>
         </div>
