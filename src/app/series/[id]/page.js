6acd05115a1735aaa7490afa6e8a89aea90fb3f6
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-14 00:01:29 +0200

    edit Filter button css

--- src/app/series/[id]/page.js
+++ src/app/series/[id]/page.js
@@ -109,25 +109,31 @@ export default function AnimeDetail({ params }) {
   }, [isMobile]);
 
   const Filter = ({ activeFilter, setActiveFilter }) => {
+    const isOrderTypeAvailable = (orderType) => {
+      return series.seriesOrder.some((order) => order.order_type === orderType);
+    };
     return (
       <div id="filters" className="flex flex-col">
         <p className="hidden sm:block text-lg font-gilroy text-subtext">Order</p>
         <div className="flex flex-row items-center justify-around w-full mb-4">
           <button
-            className={`${activeFilter === 'Release' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
+            className={`${activeFilter === 'Release' ? '' : 'btn-inactive'} btn`}
             onClick={() => setActiveFilter('Release')}
+            disabled={!isOrderTypeAvailable('Release')}
           >
             Release
           </button>
           <button
-            className={`${activeFilter === 'Chronological' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm mx-4`}
+            className={`${activeFilter === 'Chronological' ? '' : 'btn-inactive'} btn mx-2`}
             onClick={() => setActiveFilter('Chronological')}
+            disabled={!isOrderTypeAvailable('Chronological')}
           >
             Chronicle
           </button>
           <button
-            className={`${activeFilter === 'Community' ? 'btn' : 'btn-inactive'} py-2 px-4 rounded-sm`}
+            className={`${activeFilter === 'Community' ? '' : 'btn-inactive'} btn`}
             onClick={() => setActiveFilter('Community')}
+            disabled={!isOrderTypeAvailable('Community')}
           >
             Community
           </button>
