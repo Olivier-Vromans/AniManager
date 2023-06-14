ad19694ecf983bdf0d9bc223fed10afcf2e94e5f
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-14 23:05:36 +0200

    edit change image to webp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import Featured from './Featured.js';
 export default function Hero() {
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-[url('/img/bg/mobile-hero.webp')] md:bg-[url('/img/bg/itachi.png')] bg-no-repeat bg-top bg-cover md:bg-no-repeat md:bg-cover">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-[url('/img/bg/mobile-hero.webp')] md:bg-[url('/img/bg/itachi.webp')] bg-no-repeat bg-top bg-cover md:bg-no-repeat md:bg-cover">
       <div className=" w-full justify-center items-center font-mono text-sm sm:flex">
         <div className="flex flex-col lg:flex-row items-center">
           <div className="flex flex-col text-center items-center w-full p-4 sm:p-24">
