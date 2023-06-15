2e0668fe031fa8ec5daf94397469d21ccfac22ac
Olivier-Vromans <olivier.vromans@hotmail.nl>
2023-06-15 16:23:41 +0200

    Edit admin didn't fetch anymore

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,13 +1,13 @@
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]"
-import prisma from '../../prisma/prismaClient.js';
-
+import { getSession } from 'next-auth/react';
+import prisma from '../../prisma/prismaClient';
 
 export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions)
-  if(!session) {
-    res.status(401).json({message: "Unauthorized"})
+  const session = await getSession({ req });
+  if (!session) {
+    res.status(401).json({ message: 'Unauthorized' });
+    return;
   }
+
   try {
     const series = await prisma.series.findMany({
       include: {
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
           include: {
             seriesOrderItems: {
               include: {
-                animes: {
+                anime: {
                   include: {
                     genres: true,
                     episodes: true,
@@ -40,27 +40,27 @@ export default async function handler(req, res) {
       serieOrders: serie.seriesOrder
         ? serie.seriesOrder.map((order) => ({
             order_id: order.order_id,
-            Order_type: order.order_type,
-            animeOrder: order.animeOrders.map((animeOrder) => ({
-              order_index: animeOrder.order_index,
-              series_id: animeOrder.serie_id,
+            order_type: order.order_type,
+            animeOrder: order.seriesOrderItems.map((seriesOrderItem) => ({
+              order_index: seriesOrderItem.order,
+              series_id: seriesOrderItem.serie_id,
               animes: {
-                anime_id: animeOrder.animes.anime_id,
-                title: animeOrder.animes.title,
-                description: animeOrder.animes.description,
-                release_date: animeOrder.animes.release_date,
-                is_dubbed: animeOrder.animes.is_dubbed,
-                optional: animeOrder.animes.optional,
-                poster: animeOrder.animes.poster,
-                banner: animeOrder.animes.banner,
-                type: animeOrder.animes.type,
-                genres: animeOrder.animes.genres.map((genre) => ({
+                anime_id: seriesOrderItem.anime.anime_id,
+                title: seriesOrderItem.anime.title,
+                description: seriesOrderItem.anime.description,
+                release_date: seriesOrderItem.anime.release_date,
+                is_dubbed: seriesOrderItem.anime.is_dubbed,
+                optional: seriesOrderItem.anime.optional,
+                poster: seriesOrderItem.anime.poster,
+                banner: seriesOrderItem.anime.banner,
+                type: seriesOrderItem.anime.type,
+                genres: seriesOrderItem.anime.genres.map((genre) => ({
                   genre_id: genre.genre_id,
                   genre_name: genre.genre_name,
                   url: genre.url,
                   count: genre.count,
                 })),
-                episodes: animeOrder.animes.episodes.map((episode) => ({
+                episodes: seriesOrderItem.anime.episodes.map((episode) => ({
                   episode_id: episode.episode_id,
                   episode_number: episode.episode_number,
                   is_filler: episode.is_filler,
